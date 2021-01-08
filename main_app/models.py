9a4eef48859003ef5e8d8ec89b2907c493c582f4
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-01-08 11:24:23 -0800

    create widget model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Widget(models.Model):
+    description = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.description
     
+    def get_absolute_url(self):
+        return reverse('index')
